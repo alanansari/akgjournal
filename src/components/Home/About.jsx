24f4a84a8c57f0bf0b36d4ef9a2b20ffb86e507f
alanansari <ansarialan31@gmail.com>
2023-11-04 23:02:04 +0530

    ADD: about section in home

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+import { oswald } from '@/utils/fonts'
+
+const About = () => {
+  return (
+    <div id='About' className='md:pt-[15vh]'>
+        <div className='flex flex-col md:flex-row pb-10 w-[86%] mx-auto justify-start items-center'>
+          <div className={`text-5xl py-3 px-7  mb-[36px] md:mb-0 md:mr-auto font-semibold border-b-[3px] border-slate-900 ${oswald.className}`}>About Us</div>
+          <img src="about.png" alt="pic" className='w-[337px] h-[158px] object-cover rounded-xl outline outline-1 outline-offset-2'/>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -6,7 +6,7 @@ interface Props{
 
 const Textbox = ({children}:Props) => {
   return (
-    <div className='flex flex-col items-center text-center md:text-start md:items-start px-[20px] md:px-[52px] py-[56px] [&>*]:mt-5 w-[90%] mx-auto text-[#5A6473] rounded-3xl border border-[#ffda48] bg-[#fff]'>
+    <div className='flex flex-col items-center text-center md:text-start md:items-start px-[20px] md:px-[52px] py-[56px] [&>*]:mt-5 w-[90%] mx-auto text-[#5A6473] rounded-3xl border border-[#000] bg-[#fff]'>
         {children}
     </div>
   )

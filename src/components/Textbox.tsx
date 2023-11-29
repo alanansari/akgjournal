6e2732149f119749bb73ba07a6f0d65f20930b00
Archiehere <cheeku67@gmail.com>
2023-11-29 18:26:39 +0530

    Bugfix

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 const Textbox = ({children , pxval}:Props) => {
   return (
-    <div className={`flex flex-col items-center  md:text-start md:items-start px-10 px-${pxval} py-[56px] [&>*]:mt-5 w-[90%] mx-auto text-[#08090C] rounded-3xl `}>
+    <div className={`flex flex-col items-center  md:text-start md:items-start px-10 md:px-${pxval} py-[56px] [&>*]:mt-5 w-[90%] mx-auto text-[#08090C] rounded-3xl `}>
         {children}
     </div>
   )

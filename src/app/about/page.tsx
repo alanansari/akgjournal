3d4e3d6d7001ac97213a1e6a909003787e57317c
Sanika goyal <sanika2110118@akgec.ac.in>
2023-12-10 13:02:37 +0530

    list data updated in archive section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,23 +7,25 @@ import Footer from "@/components/Footer/Footer"
 
 export default function Home() {
   return (
-    <div className={openSans.className}>
-      <Navbar page='about'/>
-      <div className="px-8 md:px-20 pt-[10rem] flex md:flex-row flex-col-reverse justify-around items-center ">
-      <div className="pt-5 md:w-[40%] w-full">
-          <h1 className={`${clash_Display.className} font-semibold text-5xl w-[100%] md:w-[50%] leading-normal pb-3`}>About Us</h1>
-          <p className=" w-[100%] md:w-[70%] text-justify">{ListData.TopText}</p>
+    <div className={` ${openSans.className} min-h-[100vh] flex flex-col `}>
+      <Navbar page='about' />
+      <div className="flex-1">
+        <div className="px-8 md:px-20 pt-[10rem] flex md:flex-row flex-col-reverse justify-around items-center ">
+          <div className="pt-5 md:w-[40%] w-full">
+            <h1 className={`${clash_Display.className} font-semibold text-5xl w-[100%] md:w-[50%] leading-normal pb-3`}>About Us</h1>
+            <p className=" w-[100%] md:w-[70%] text-justify">{ListData.TopText}</p>
+          </div>
+          <img src="/about.png" alt="pic" className='w-[40rem] h-[20rem] object-cover' />
         </div>
-        <img src="/about.png" alt="pic" className='w-[40rem] h-[20rem] object-cover'/>
+        <Textbox>
+          {ListData.List.map((item, index) => {
+            return (
+              <li key={index}>{item}</li>
+            )
+          })}
+
+        </Textbox>
       </div>
-      <Textbox>
-        {ListData.List.map((item, index) => {
-          return (
-            <li key={index}>{item}</li>
-          )
-        })}
-        
-      </Textbox>
       <Footer />
     </div>
   )
